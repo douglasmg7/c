b336de01e0cadfdb6becc4e5ea27ac00f3fca2c7
douglasmg7 <douglasmg7@gmail.com>
2020-05-01 08:56:08 -0300

    data types

---
+++ data-type.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+    Data type is used to declare the type of a variable.
+    Data type determine the type and size of data associated with a variable.
+
+*/
+int main(void) {
+    printf("Hello world!\n");
+}
