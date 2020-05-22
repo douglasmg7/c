65c6059f56c4c240933fad83c31ad29f6470625a
douglasmg7 <douglasmg7@gmail.com>
2020-05-22 15:57:21 -0300

    recursion

--- st-course/data-type.c
+++ st-course/data-type.c
@@ -8,10 +8,10 @@ int main(void) {
     printf(":: Values in byte\n");
     // This is a integer data tupe used to store a single character(ASCII code) value or 1 byte of signed or unsigned integer value.
     // There is no other special meaning for the char type, and it is just another integer data type.
-    printf("char: %lu\n", sizeof(char));
-    printf("short: %lu\n", sizeof(short));
-    printf("int: %lu\n", sizeof(int));
-    printf("long: %lu\n", sizeof(long));
+    printf("char: %lu\n", sizeof(char));    // Always 1 byte.
+    printf("short: %lu\n", sizeof(short));  // Always 2 bytes.
+    printf("int: %lu\n", sizeof(int));      // Depends on compile implementation, can be 2 or 4 bytes.
+    printf("long: %lu\n", sizeof(long));    // Depends on compile implementation, can be 4 or 8 bytes.
     printf("long long: %lu\n", sizeof(long long));
 
     // Variable definition (data type + variable name).
