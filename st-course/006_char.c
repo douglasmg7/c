1f312cfdc16f9f766b4fefb4a25137bf221a7446
douglasmg7 <douglasmg7@gmail.com>
2026-04-08 08:16:53 -0300

    ASCII print

---
+++ st-course/006_char.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char v1 = 'A';
+    char v2 = 66;
+
+    printf("Value of v1: %d, character: %c\n", v1, v1);
+    printf("Value of v2: %d, character: %c\n", v2, v2);
+
+    return 0;
+}
